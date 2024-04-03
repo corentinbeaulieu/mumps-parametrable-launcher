ef62ae7855294b12e5f34fbe8dd3cc229dbb0339
Corentin Beaulieu <corentin.beaulieu@ens.uvsq.fr>
2024-04-03 15:38:14 +0200

    Add support for complex MatrixMarket matrix

--- MUMPS/Base/mumps.c
+++ MUMPS/Base/mumps.c
@@ -1,9 +1,14 @@
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <omp.h>
 #include <mpi.h>
 #include <string.h>
+#include <zmumps_c.h>
 #include <dmumps_c.h>
+
 #define JOB_INIT -1
 #define JOB_END -2
 #define USE_COMM_WORLD -987654
@@ -23,17 +28,29 @@ typedef enum {
 } parse_error_t;
 
 /** MUMPS values for symmetry **/
-typedef enum {
+typedef enum : int {
     Unsymmetric = 0,
     SymmetricDefinePositive = 1,
     Symmetric = 2
 } specificity_t;
 
+/** Generic matrix for both double and double complex **/
+typedef struct {
+    union {
+        DMUMPS_REAL *d_array;
+        ZMUMPS_COMPLEX *z_array;
+    };
+    enum : unsigned char {
+        d,
+        z
+    } type;
+} matrix_t;
+
 /** Parses a matrix formatted with the MatrixMarket format and stores it in well-formed arrays for MUMPS
  *  Note that only a subpart of the format is supported (the one we need currently)
  */
 parse_error_t parseMTX (MUMPS_INT *n, MUMPS_INT8 *nnz, MUMPS_INT **irn, MUMPS_INT **jcn, 
-                double **a, specificity_t *spec, char *input_file) {
+                        matrix_t *a, specificity_t *spec, char *input_file) {
 
     FILE *const fd = fopen(input_file, "r");
     if (fd == NULL) {
@@ -61,7 +78,13 @@ parse_error_t parseMTX (MUMPS_INT *n, MUMPS_INT8 *nnz, MUMPS_INT **irn, MUMPS_IN
         return UnknownFormat;
     }
 
-    if (strcmp(type, "real") != 0) {
+    if (strcmp(type, "real") == 0) {
+        a->type = d;
+    }
+    else if  (strcmp(type, "complex") == 0) {
+        a->type = z;
+    }
+    else {
         return UnknownType;
     }
 
@@ -81,19 +104,31 @@ parse_error_t parseMTX (MUMPS_INT *n, MUMPS_INT8 *nnz, MUMPS_INT **irn, MUMPS_IN
 
     // Get the matrix characteristics
     size_t tmp;
-    sscanf(buffer, "%u %lu %lu", n, &tmp, nnz);
+    sscanf(buffer, "%d %lu %ld", n, &tmp, nnz);
 
     *irn = (int *) malloc(*nnz * sizeof(int));
     *jcn = (int *) malloc(*nnz * sizeof(int));
-    *a = (double *) malloc(*nnz * sizeof(double));
+    if (a->type == d) {
+        a->d_array = (DMUMPS_REAL *) malloc(*nnz * sizeof(DMUMPS_REAL));
+        if (*irn == NULL || *jcn == NULL || a->d_array == NULL) return MallocError;
 
-    if (*irn == NULL || *jcn == NULL || *a == NULL) return MallocError;
-    
-    // Get the elements of the matrix
-    size_t i = 0;
-    while(getline(&buffer, &buffer_size, fd) > 0 && i < *nnz) {
-        sscanf(buffer, "%u %u %lf", *irn + i, *jcn + i, *a + i);
-        i++;
+        // Get the elements of the matrix
+        MUMPS_INT8 i = 0;
+        while(getline(&buffer, &buffer_size, fd) > 0 && i < *nnz) {
+            sscanf(buffer, "%d %d %lf", *irn + i, *jcn + i, a->d_array + i);
+            i++;
+        }
+    }
+    else {
+        a->z_array = (ZMUMPS_COMPLEX *) malloc(*nnz * sizeof(ZMUMPS_COMPLEX));
+        if (*irn == NULL || *jcn == NULL || a->z_array == NULL) return MallocError;
+
+        // Get the elements of the matrix
+        MUMPS_INT8 i = 0;
+        while(getline(&buffer, &buffer_size, fd) > 0 && i < *nnz) {
+            sscanf(buffer, "%d %d %lf %lf", *irn + i, *jcn + i, &(a->z_array[i].r), &(a->z_array[i].i));
+            i++;
+        }
     }
 
     fclose(fd);
@@ -102,22 +137,39 @@ parse_error_t parseMTX (MUMPS_INT *n, MUMPS_INT8 *nnz, MUMPS_INT **irn, MUMPS_IN
     return Success;
 }
 
-/** Generates a rhs in order to have the desired x upon resolving the system
- *  This function aims to ease the verification of the result
+/** Generates a rhs that will give the desired x upon resolution
  *  WARNING: work in progress
  */
-void generate_rhs (MUMPS_INT n, MUMPS_INT8 nnz, MUMPS_INT irn[static nnz],
-                   double a[static n], double rhs[static n]) {
+void dgenerate_rhs (MUMPS_INT n, MUMPS_INT8 nnz, MUMPS_INT irn[static nnz],
+                    DMUMPS_REAL a[static nnz], DMUMPS_REAL rhs[static n]) {
 
     memset(rhs, 0, n * sizeof(double));
 
     // We want to have the unit vector as solution
-    for (size_t k = 0; k < nnz; k++) {
-        const size_t i = irn[k];
+    for (MUMPS_INT8 k = 0; k < nnz; k++) {
+        const MUMPS_INT i = irn[k];
 
         rhs[i] += a[k];
     }
 }
+
+void zgenerate_rhs (MUMPS_INT n, MUMPS_INT8 nnz, MUMPS_INT irn[static nnz],
+                    ZMUMPS_COMPLEX a[static nnz], ZMUMPS_COMPLEX rhs[static n]) {
+
+    memset(rhs, 0, n * sizeof(ZMUMPS_COMPLEX));
+
+    // We want to have the unit vector as solution
+    for (MUMPS_INT8 k = 0; k < nnz; k++) {
+        const MUMPS_INT i = irn[k];
+
+        rhs[i].r += a[k].r;
+        rhs[i].i += a[k].i;
+    }
+}
+
+#define generate_rhs(n, nnz, irn, a, rhs) _Generic(a, \
+        DMUMPS_REAL *: dgenerate_rhs, \
+        ZMUMPS_COMPLEX *: zgenerate_rhs)(n, nnz, irn, a, rhs)
 
 int main (int argc, char *argv[]) {
 
@@ -131,10 +183,10 @@ int main (int argc, char *argv[]) {
     MUMPS_INT8 nnz;
     MUMPS_INT *irn;
     MUMPS_INT *jcn;
-    double *a;
+    matrix_t a;
     specificity_t spec;
 
-    parse_error_t perr = parseMTX(&n, &nnz, &irn, &jcn, &a, &spec,argv[1]);
+    parse_error_t perr = parseMTX(&n, &nnz, &irn, &jcn, &a, &spec, argv[1]);
     switch(perr) {
         case NotMatrixMarket:
             fprintf(stderr, "Not a Matrix Market input!\n");
@@ -143,7 +195,7 @@ int main (int argc, char *argv[]) {
             fprintf(stderr, "Second description term must be matrix\n");
             return perr;
         case UnknownType:
-            fprintf(stderr, "Only real numbers are supported\n");
+            fprintf(stderr, "Only real/complex numbers are supported\n");
             return perr;
         case UnknownFormat:
             fprintf(stderr, "Only coordinate format is allowed\n");
@@ -161,58 +213,121 @@ int main (int argc, char *argv[]) {
             break;
     }
 
-    double *rhs = malloc(n * sizeof(double));
-
-    generate_rhs(n, nnz, irn, a, rhs);
 
     int ierr, rank;
     DMUMPS_STRUC_C dmumps_par;
+    ZMUMPS_STRUC_C zmumps_par;
 
-    dmumps_par.par = (int) atoi(argv[2]);
+    const int par = (int) atoi(argv[2]);
     const MUMPS_INT icntl_13 = (MUMPS_INT) atoi(argv[3]);
     const MUMPS_INT icntl_16 = (MUMPS_INT) atoi(argv[4]);
 
-    // This parameter controls the involvement of the root rank in the factorization
-    dmumps_par.ICNTL(13) = icntl_13;
-    // This parameter explicitly request a number of OMP threads
-    dmumps_par.ICNTL(16) = icntl_16;
 
-    // Initialize MPI
-    ierr = MPI_Init(&argc, &argv);
-    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (a.type == d) {
 
-    // Initialize MUMPS
-    dmumps_par.comm_fortran = USE_COMM_WORLD;
-    dmumps_par.job = JOB_INIT;
+        DMUMPS_REAL *rhs = malloc(n * sizeof(DMUMPS_REAL));
 
-    dmumps_c(&dmumps_par);
+        generate_rhs(n, nnz, irn, a.d_array, rhs);
 
-    // Define the system 
-    if(rank == 0) {
-        dmumps_par.n = n;
-        dmumps_par.nnz = nnz;
-        dmumps_par.irn = irn;
-        dmumps_par.jcn = jcn;
-        dmumps_par.a = a;
-        dmumps_par.rhs = rhs;
-        dmumps_par.sym = (int) spec;
+        // Initialize MPI
+        ierr = MPI_Init(&argc, &argv);
+        ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        // Initialize MUMPS
+        dmumps_par.comm_fortran = USE_COMM_WORLD;
+        dmumps_par.job = JOB_INIT;
+
+        // This parameter controls the involvement of the root rank in the factorization
+        // and solve phase
+        dmumps_par.par = par;
+        // This parameter controls the parallelism of factorization of the root node
+        dmumps_par.ICNTL(13) = icntl_13;
+        // This parameter explicitly request a number of OMP threads
+        dmumps_par.ICNTL(16) = icntl_16;
+
+        dmumps_c(&dmumps_par);
+
+        // Define the system
+        if(rank == 0) {
+            dmumps_par.n = n;
+            dmumps_par.nnz = nnz;
+            dmumps_par.irn = irn;
+            dmumps_par.jcn = jcn;
+            dmumps_par.a = a.d_array;
+            dmumps_par.rhs = rhs;
+            dmumps_par.sym = (int) spec;
+        }
+
+        // Enables residual computation and printing
+        dmumps_par.ICNTL(11) = 2;
+        dmumps_par.ICNTL(13) = icntl_13;
+        // This parameter controls the memory relaxation of the MUMPS during factorisation
+        // We need to increase it as some matrix needs large size of temporary memory
+        dmumps_par.ICNTL(14) = 25;
+        dmumps_par.ICNTL(16) = icntl_16;
+
+        // Launch MUMPS for Analysis, Factorisation and Resolution
+        dmumps_par.job = 6;
+        dmumps_c(&dmumps_par);
+
+        // Deinitialize MUMPS
+        dmumps_par.job = JOB_END;
+        dmumps_c(&dmumps_par);
+
+        free(rhs);
     }
+    else {
+        ZMUMPS_COMPLEX *rhs = malloc(n * sizeof(ZMUMPS_COMPLEX));
 
-    // Enables residual computation and printing
-    dmumps_par.ICNTL(11) = 2;
-    dmumps_par.ICNTL(13) = icntl_13;
-    // This parameter controls the memory relaxation of the MUMPS during factorisation
-    // We need to increase it as some matrix needs large size of temporary memory
-    dmumps_par.ICNTL(14) = 25;
-    dmumps_par.ICNTL(16) = icntl_16;
+        generate_rhs(n, nnz, irn, a.z_array, rhs);
 
-    // Launch MUMPS for Analysis, Factorisation and Resolution
-    dmumps_par.job = 6;
-    dmumps_c(&dmumps_par);
+        const MUMPS_INT icntl_13 = (MUMPS_INT) atoi(argv[3]);
+        const MUMPS_INT icntl_16 = (MUMPS_INT) atoi(argv[4]);
 
-    // Deinitialize MUMPS
-    dmumps_par.job = JOB_END;
-    dmumps_c(&dmumps_par);
+
+        // Initialize MPI
+        ierr = MPI_Init(&argc, &argv);
+        ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        // Initialize MUMPS
+        zmumps_par.comm_fortran = USE_COMM_WORLD;
+        zmumps_par.job = JOB_INIT;
+
+        zmumps_par.par = par;
+        zmumps_par.ICNTL(13) = icntl_13;
+        zmumps_par.ICNTL(16) = icntl_16;
+
+        zmumps_c(&zmumps_par);
+
+        // Define the system
+        if(rank == 0) {
+            zmumps_par.n = n;
+            zmumps_par.nnz = nnz;
+            zmumps_par.irn = irn;
+            zmumps_par.jcn = jcn;
+            zmumps_par.a = a.z_array;
+            zmumps_par.rhs = rhs;
+            zmumps_par.sym = (int) spec;
+        }
+
+        // Enables residual computation and printing
+        zmumps_par.ICNTL(11) = 2;
+        zmumps_par.ICNTL(13) = icntl_13;
+        // This parameter controls the memory relaxation of the MUMPS during factorisation
+        // We need to increase it as some matrix needs large size of temporary memory
+        zmumps_par.ICNTL(14) = 25;
+        zmumps_par.ICNTL(16) = icntl_16;
+
+        // Launch MUMPS for Analysis, Factorisation and Resolution
+        zmumps_par.job = 6;
+        zmumps_c(&zmumps_par);
+
+        // Deinitialize MUMPS
+        zmumps_par.job = JOB_END;
+        zmumps_c(&zmumps_par);
+
+        free(rhs);
+    }
 
     // Print the solution for verification
     // TODO: Automate the verification (residual, forward error if possible...)
@@ -229,8 +344,7 @@ int main (int argc, char *argv[]) {
 
     free(irn);
     free(jcn);
-    free(a);
-    free(rhs);
+    free(a.d_array);
 
     return 0;
 }
